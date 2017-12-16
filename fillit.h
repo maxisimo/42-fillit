2e59d228ed58da0ee7eb2aef5d073501875bba93
maxisimo <34480775+maxisimo@users.noreply.github.com>
2017-12-16 11:58:35 +0100

    update fillit.h

--- fillit.h
+++ fillit.h
@@ -31,7 +31,7 @@ typedef struct	s_etris
 	char		**pos;
 	int			width;
 	int			height;
-	char		value;
+	char		val;
 }				t_etris;
 
 void			min_max(char *str, t_point *min, t_point *max);
@@ -41,7 +41,7 @@ void			set_piece(t_etris *tetri, t_map *map, t_point *point, char c);
 void			free_tetris(t_etris *tetri);
 
 t_etris			*get_piece(char *str, char value);
-t_etris			*new_tetri(char **pos, int width, int height, char value);
+t_etris			*new_tetri(char **pos, int width, int height, char val);
 t_list			*read_tetri(int fd, char *buf, char lettre);
 t_list			*free_list(t_list *list);
 t_map			*new_map(int size);
@@ -51,7 +51,7 @@ t_point			*new_point(int x, int y);
 int				check_connection(char *str);
 int				main(int argc, char **argv);
 int				counts(char *str, int count);
-int				place(t_etris *tetri, t_map *map, int x, int y);
+int				placement(t_etris *tetri, t_map *map, int x, int y);
 int				solve_map(t_map *map, t_list *list);
 int				ft_sqrt(int n);
 
